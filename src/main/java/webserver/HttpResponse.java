97d359f40d9c454604f647ee3e7949faf830ab2b
yohwan <dldyghks951@gmail.com>
2023-07-17 20:22:40 +0900

    <feat> HttpResponse 구현 : http 응답 메세지

---
+++ src/main/java/webserver/HttpResponse.java
@@ -0,0 +1,47 @@
+package webserver;
+
+import java.util.Map;
+
+/*
+* response 객체 만들고,
+* */
+public class HttpResponse {
+    private String version;
+    private StatusCode statusCode;
+
+    private Map<String, String> metadata;
+    private String body;
+
+    public HttpResponse(String version, StatusCode statusCode, Map<String, String> metadata, String body) {
+        this.version = version;
+        this.statusCode = statusCode;
+        this.metadata = metadata;
+        this.body = body;
+    }
+
+    //todo : default metadata 로직
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public StatusCode getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(StatusCode statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public Map<String, String> getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Map<String, String> metadata) {
+        this.metadata = metadata;
+    }
+}
