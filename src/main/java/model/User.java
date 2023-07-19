02cf767433436639505ac9342038d96fa64a3524
yohwan <dldyghks951@gmail.com>
2023-07-20 00:01:54 +0900

    <feat> User - User 객체 비교를 위해 equals, hashcode 구현

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String userId;
@@ -38,6 +39,19 @@ public class User {
         String email = parameters.get("email");
 
         return new User(userId, password, name, email);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId) && Objects.equals(password, user.password) && Objects.equals(name, user.name) && Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, password, name, email);
     }
 
     @Override
