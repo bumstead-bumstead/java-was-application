0e81988039eddcb98eec09f0074de9b409a4cbe1
yohwan <dldyghks951@gmail.com>
2023-07-20 00:02:38 +0900

    <feat> 요청을 처리하는 Controller의 메소드들을 식별하기 위해 HandleRequest 애너테이션 구현

---
+++ src/main/java/webserver/annotations/HandleRequest.java
@@ -0,0 +1,15 @@
+package webserver.annotations;
+
+import webserver.httpMessage.HttpMethod;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME) //컴파일 시에만 필요한 것 맞는지?
+public @interface HandleRequest {
+    String path();
+    HttpMethod httpMethod();
+}
