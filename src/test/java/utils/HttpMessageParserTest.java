dd09478b6042bb2bbb7b4b4f8fb4f6cc966c9b0f
yohwan <dldyghks951@gmail.com>
2023-07-19 13:19:07 +0900

    <test> HttpMessageParserTest - 쿼리 파싱 테스트 코드 구현

--- src/test/java/utils/HttpMessageParserTest.java
+++ src/test/java/utils/HttpMessageParserTest.java
@@ -1,7 +1,8 @@
 package utils;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import webserver.message.*;
+import webserver.httpMessage.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -11,12 +12,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.in;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HttpMessageParserTest {
 
     @Test
+    @DisplayName("HttpResponse to OutputStream 파싱 로직 테스트")
     void parseHttpResponse() throws IOException {
         //given
         HttpResponse httpResponse = new HttpResponse(StatusCode.OK, new byte[0]);
@@ -32,12 +33,14 @@ class HttpMessageParserTest {
     }
 
     @Test
+    @DisplayName("InputStream to HttpRequest 파싱 로직 테스트")
     void parseHttpRequest() throws IOException {
         //given
         String input = "GET /index.html HTTP/1.1\n" +
                 "Host: localhost:8080\n" +
                 "Connection: keep-alive\n" +
                 "Cache-Control: max-age=0\n";
+
         Map<String, String> expectedHeader = new HashMap<>();
         expectedHeader.put("Host", "localhost:8080");
         expectedHeader.put("Connection", "keep-alive");
@@ -46,6 +49,33 @@ class HttpMessageParserTest {
         InputStream inputStream = new ByteArrayInputStream(input.getBytes());
         HttpRequest expected = new HttpRequest(HttpMethod.GET,
                 new URI("/index.html", Map.of()),
+                "HTTP/1.1",
+                expectedHeader);
+
+        //when
+        HttpRequest actual = HttpMessageParser.parseHttpRequest(inputStream);
+
+        //then
+        assertTrue(actual.equals(expected));
+    }
+
+    @Test
+    @DisplayName("쿼리가 있는 경우 InputStream to HttpRequest 파싱 로직 테스트")
+    void parseHttpRequestWithQuery() throws IOException {
+        //given
+        String input = "GET /user/create?key1=value1&key2=value2 HTTP/1.1\n" +
+                "Host: localhost:8080\n" +
+                "Connection: keep-alive\n" +
+                "Cache-Control: max-age=0\n";
+
+        Map<String, String> expectedHeader = new HashMap<>();
+        expectedHeader.put("Host", "localhost:8080");
+        expectedHeader.put("Connection", "keep-alive");
+        expectedHeader.put("Cache-Control", "max-age=0");
+
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        HttpRequest expected = new HttpRequest(HttpMethod.GET,
+                new URI("/user/create", Map.of("key1", "value1", "key2", "value2")),
                 "HTTP/1.1",
                 expectedHeader);
 
