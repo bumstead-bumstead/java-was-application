8116fc1038dcc5340f60fed1a4da32698a01db21
yohwan <dldyghks951@gmail.com>
2023-07-21 00:22:51 +0900

    <test> StaticResourceHandlerTest - DisplayName 작성

--- src/test/java/webserver/StaticResourceHandlerTest.java
+++ src/test/java/webserver/StaticResourceHandlerTest.java
@@ -1,8 +1,10 @@
 package webserver;
 
+import exceptions.PathNotFoundException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import webserver.handlers.StaticResourceHandler;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,17 +12,19 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StaticResourceHandlerTest {
 
     @Test
-    public void testGetStaticResource_ExistingResource() throws IOException {
-        String resourcePath = "/exampleForTest.txt"; // 존재하는 리소스 파일 경로
+    @DisplayName("존재하는 경로의 정적 리소스의 경우 바이트 파일을 반환한다.")
+    public void getStaticResourceExisting() throws IOException {
+        String resourcePath = "/exampleForTest.txt";
         byte[] content = StaticResourceHandler.getStaticResource(resourcePath);
         assertNotNull(content);
         assertTrue(content.length > 0);
     }
 
     @Test
-    public void testGetStaticResource_NonExistingResource() {
-        String resourcePath = "/non_existing.txt"; // 존재하지 않는 리소스 파일 경로
-        assertThrows(FileNotFoundException.class, () -> {
+    @DisplayName("존재하지 않는 경로의 정적 리소스 요청은 PathNotFoundException을 발생시킨다.")
+    public void getStaticResourceNonExisting() {
+        String resourcePath = "/non_existing.txt";
+        assertThrows(PathNotFoundException.class, () -> {
             StaticResourceHandler.getStaticResource(resourcePath);
         });
     }
