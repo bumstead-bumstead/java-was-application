1fd66526fb83fe9664dc116819e0c8739b0c911d
yohwan <dldyghks951@gmail.com>
2023-07-22 15:13:27 +0900

    <test> 이미 데이터베이스에 존재하는 유저인지 검증하는 기능에 대한 Database 테스트 코드 구현

---
+++ src/test/java/db/DatabaseTest.java
@@ -0,0 +1,38 @@
+package db;
+
+import exceptions.BadRequestException;
+import model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class DatabaseTest {
+
+    private User user;
+
+    @BeforeEach
+    void setUp() throws BadRequestException {
+        Map<String, String> userParameters = new HashMap<>();
+        userParameters.put("userId", "john_doe");
+        userParameters.put("password", "secret_password");
+        userParameters.put("name", "John Doe");
+        userParameters.put("email", "john.doe@example.com");
+
+        user = User.of(userParameters);
+    }
+
+    @Test
+    @DisplayName("이미 존재하는 유저일 경우 BadRequestException을 발생시킨다.")
+    void addUser() throws BadRequestException {
+        Database.addUser(user);
+        assertThrows(BadRequestException.class, () -> Database.addUser(user));
+    }
+
+
+
+}
