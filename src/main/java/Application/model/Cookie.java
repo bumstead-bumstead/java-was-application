0ca97c16cc875fae537a1b6468d3f17742a7222b
yohwan <dldyghks951@gmail.com>
2023-07-23 20:19:38 +0900

    <feat> Cookie 생성자 추가

--- src/main/java/Application/model/Cookie.java
+++ src/main/java/Application/model/Cookie.java
@@ -4,6 +4,11 @@ public class Cookie {
     private String value;
     private String userId;
 
+    public Cookie(String value, String userId) {
+        this.value = value;
+        this.userId = userId;
+    }
+
     public String getValue() {
         return value;
     }
