f0a6463ee0d85120f9bc389c80e0141333b0aa9c
yohwan <dldyghks951@gmail.com>
2023-07-23 20:22:18 +0900

    <feat> HttpHeaderUtils - 쿠키 헤더 형식 문자열 반환 기능 구현

--- src/main/java/webserver/utils/HttpHeaderUtils.java
+++ src/main/java/webserver/utils/HttpHeaderUtils.java
@@ -5,4 +5,21 @@ public class HttpHeaderUtils {
     public static final String CONTENT_LENGTH_HEADER = "Content-Length";
     public static final String DEFAULT_HTTP_VERSION = "HTTP/1.1";
     public static final String LOCATION_HEADER = "Location";
+    public static final String SET_COOKIE_HEADER = "Set-Cookie";
+    public static final String PATH_HEADER = "path=";
+    public static final String COOKIE_VALUE_PREFIX = "sid=";
+    public static final int COOKIE_LENGTH = 10;
+
+    public static String generateCookieHeader() {
+        String cookie = RandomUtils.generateRandomString(COOKIE_LENGTH);
+
+        StringBuilder cookieValue = new StringBuilder()
+                .append(COOKIE_VALUE_PREFIX)
+                .append(cookie)
+                .append("; ")
+                .append(PATH_HEADER)
+                .append("/");
+
+        return cookieValue.toString();
+    }
 }
