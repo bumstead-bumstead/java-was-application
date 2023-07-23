930fcd020dec1e3dfccab9b44ddd529bda092b60
yohwan <dldyghks951@gmail.com>
2023-07-23 20:18:24 +0900

    <feat> Cookie - Cookie 객체를 저장하기 위한 모델 구현

---
+++ src/main/java/Application/model/Cookie.java
@@ -0,0 +1,22 @@
+package Application.model;
+
+public class Cookie {
+    private String value;
+    private String userId;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
