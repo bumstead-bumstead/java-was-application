2a6ea705b6a05fda13d147168c33f8b94a9515d6
yohwan <dldyghks951@gmail.com>
2023-07-24 22:16:06 +0900

    <chore> 불필요한 메소드 삭제

--- src/main/java/webserver/utils/HttpHeaderUtils.java
+++ src/main/java/webserver/utils/HttpHeaderUtils.java
@@ -6,20 +6,8 @@ public class HttpHeaderUtils {
     public static final String DEFAULT_HTTP_VERSION = "HTTP/1.1";
     public static final String LOCATION_HEADER = "Location";
     public static final String SET_COOKIE_HEADER = "Set-Cookie";
+    public static final String REQUEST_COOKIE_HEADER = "Cookie";
     public static final String PATH_HEADER = "path=";
     public static final String COOKIE_VALUE_PREFIX = "sid=";
     public static final int COOKIE_LENGTH = 10;
-
-    public static String generateCookieHeader() {
-        String cookie = RandomUtils.generateRandomString(COOKIE_LENGTH);
-
-        StringBuilder cookieValue = new StringBuilder()
-                .append(COOKIE_VALUE_PREFIX)
-                .append(cookie)
-                .append("; ")
-                .append(PATH_HEADER)
-                .append("/");
-
-        return cookieValue.toString();
-    }
 }
