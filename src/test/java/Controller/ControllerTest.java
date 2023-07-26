22f8c325579648655cfb9da0cb83e0aea95e21a3
yohwan <dldyghks951@gmail.com>
2023-07-26 10:24:29 +0900

    <test> ControllerTest - index.html 동적 렌더링 테스트 코드 구현

--- src/test/java/Controller/ControllerTest.java
+++ src/test/java/Controller/ControllerTest.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import application.Controller.Controller;
+import webserver.http.session.Session;
 import webserver.http.session.SessionDatabase;
 import application.db.UserDatabase;
 import webserver.http.session.Cookie;
@@ -26,6 +27,7 @@ class ControllerTest {
         String password;
         String name;
         String email;
+
         @BeforeEach
         void setup() {
             UserDatabase.clear();
@@ -65,6 +67,7 @@ class ControllerTest {
         String password;
         String name;
         String email;
+
         @BeforeEach
         void setup() {
             UserDatabase.clear();
@@ -105,6 +108,7 @@ class ControllerTest {
             Cookie cookie = headers.getCookies().get(0);
             assertThat(cookie.getName()).isEqualTo("sid");
         }
+
         @Test
         @DisplayName("존재하지 않는 id인 경우, /user/login_failed.html로 redirection")
         void loginWithWrongId() throws BadRequestException {
@@ -128,6 +132,77 @@ class ControllerTest {
             String actualRedirectionURI = httpResponse.getHeaders().getValue(HttpHeaderUtils.LOCATION_HEADER);
 
             assertThat(actualRedirectionURI).isEqualTo(expectedRedirectionURI);
+        }
+    }
+
+    @Nested
+    @DisplayName("index.html 테스트")
+    class index {
+        Controller controller;
+        String userId;
+        String password;
+        String name;
+        String email;
+
+        @BeforeEach
+        void setup() throws BadRequestException {
+            UserDatabase.clear();
+            controller = Controller.getInstance();
+            userId = "userId";
+            password = "password";
+            name = "name";
+            email = "asdsda@dasads";
+
+            controller.createUser(userId, password, name, email);
+            controller.login(userId, password);
+        }
+
+        @Test
+        @DisplayName("유효한 세션이 있는 경우 로그아웃/개인정보/이름을 보여준다.")
+        void indexWithSession() throws Exception {
+            Session session = new Session();
+            session.addAttribute("userId", "userId");
+            SessionDatabase.addSession(session);
+
+            HttpResponse httpResponse = controller.index(session);
+            String body = new String(httpResponse.getBody());
+
+            assertThat(body).contains("name");
+            assertThat(body).contains("로그아웃");
+            assertThat(body).contains("개인정보수정");
+            assertThat(body).doesNotContain("로그인");
+            assertThat(body).doesNotContain("회원가입");
+
+        }
+
+        @Test
+        @DisplayName("유효한 세션이 없는 경우 로그인/회원가입을 보여준다.")
+        void indexWithoutSession() throws Exception {
+            HttpResponse httpResponse = controller.index(null);
+            String body = new String(httpResponse.getBody());
+
+            assertThat(body).contains("로그인");
+            assertThat(body).contains("회원가입");
+            assertThat(body).doesNotContain("로그아웃");
+            assertThat(body).doesNotContain("개인정보조회");
+
+        }
+
+        @Test
+        @DisplayName("유효하지 않은 세션이 있는 경우 로그인/회원가입을 보여준다.")
+        void indexWithInvalidSession() throws Exception {
+            Session session = new Session();
+            session.addAttribute("userId", "d");
+            SessionDatabase.addSession(session);
+
+            HttpResponse httpResponse = controller.index(session);
+
+            String body = new String(httpResponse.getBody());
+
+            assertThat(body).contains("로그인");
+            assertThat(body).contains("회원가입");
+            assertThat(body).doesNotContain("로그아웃");
+            assertThat(body).doesNotContain("개인정보조회");
         }
     }
 }
